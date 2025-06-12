e819d6ffdcd32a03cde1b70238e7c11ae9c99201
josephajibodu <josephajibodu@gmail.com>
2025-06-12 12:36:38 +0100

    email not queued

--- app/Notifications/NewBookingNotification.php
+++ app/Notifications/NewBookingNotification.php
@@ -8,9 +8,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewBookingNotification extends Notification implements ShouldQueue
+class NewBookingNotification extends Notification
 {
-    use Queueable;
+//    use Queueable;
 
     /**
      * Create a new notification instance.
