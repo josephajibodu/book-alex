f07ffbfeae1ef5e5dbafd2f1865a2ee8c52fdd1b
josephajibodu <josephajibodu@gmail.com>
2025-12-06 23:29:18 +0100

    bundle favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,8 +3,8 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
